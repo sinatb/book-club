1963614c03a72aba5735d39ce6601e5fa6d1149b
sinatb <sinatb.dev@gmail.com>
2024-08-07 13:18:18 +0330

    bookclub-011 : added empty views for tdd

---
+++ bookclubapi/apiviews.py
@@ -0,0 +1,53 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class BookListAPIView(APIView):
+
+    def get(self, request):
+        return Response({})
+
+    def post(self, request):
+        return Response({})
+
+
+class BookDetailAPIView(APIView):
+
+    def get(self, request, pk):
+        return Response({})
+
+    def delete(self, request, pk):
+        return Response({})
+
+
+class CommentListAPIView(APIView):
+
+    def post(self, request, pk):
+        return Response({})
+
+
+class CommentDetailAPIView(APIView):
+
+    def get(self, request, pk):
+        return Response({})
+
+    def delete(self, request, pk):
+        return Response({})
+
+
+class ReporterListAPIView(APIView):
+
+    def post(self, request):
+        return Response({})
+
+
+class CommentsReportListAPIView(APIView):
+    def get(self, request, pk):
+        return Response({})
+
+
+class BooksCommentListAPIView(APIView):
+
+    def get(self, request, pk):
+        return Response({})
+
