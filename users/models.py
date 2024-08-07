3d0d8e5c6bd08987742233f53ba4d46d197dbca8
sinatb <sinatb.dev@gmail.com>
2024-08-07 10:30:41 +0330

    bookclub-003: created user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+
+class User(AbstractUser):
+
+    USER_TYPE_CHOICES = (
+        ("basic", "Basic"),
+        ("publisher", "Publisher")
+    )
+
+    user_type = models.CharField(_("User Type"), choices=USER_TYPE_CHOICES)
+    email = models.CharField(_("Email Address"), max_length=255, unique=True)
+
+    def is_basic(self):
+        return self.user_type == "basic"
+
+    def is_publisher(self):
+        return self.user_type == "publisher"
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
