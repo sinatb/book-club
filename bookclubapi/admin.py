6d0cb5f20b796b382d97d181f727206279b56a81
sinatb <sinatb.dev@gmail.com>
2024-08-07 13:19:56 +0330

    bookclub-012 : added models to admin

--- bookclubapi/admin.py
+++ bookclubapi/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import User, Book, Report, Comment, Like
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Book)
+admin.site.register(Report)
+admin.site.register(Comment)
+admin.site.register(Like)
