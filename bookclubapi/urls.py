21635d4df1e09d7e68e0782991e34c8585fdb941
sinatb <sinatb.dev@gmail.com>
2024-08-08 17:33:26 +0330

    bookclub-018 : refactored url names

--- bookclubapi/urls.py
+++ bookclubapi/urls.py
@@ -3,12 +3,12 @@ from .apiviews import (BookList, BookDetail, CommentCreate, CommentDetail, Repor
                        get_book_comments, get_comment_reports, CreateLike)
 
 urlpatterns = [
-    path('/books/', BookList.as_view(), 'GET all books'),
-    path('/books/<int:pk>/', BookDetail.as_view(), 'GET book with id'),
-    path('/comments/', CommentCreate.as_view(), 'Create a Comment'),
-    path('/comments/<int:pk>/', CommentDetail.as_view(), 'DELETE or GET a comment'),
-    path('/reports/', ReportCreate.as_view(), 'Create a Report'),
-    path('/comments/<int:pk>/reports/', get_comment_reports, 'GET list of comment reports'),
-    path('/books/<int:pk>/comments/', get_book_comments, 'GET list of comments'),
-    path('/books/<int:pk>/like/', CreateLike.as_view(), 'Create a Like'),
+    path('/books/', BookList.as_view(), name='get_book_list'),
+    path('/books/<int:pk>/', BookDetail.as_view(), name='get_book_detail'),
+    path('/comments/', CommentCreate.as_view(), name='create_comment'),
+    path('/comments/<int:pk>/', CommentDetail.as_view(), name='get_comment_detail'),
+    path('/reports/', ReportCreate.as_view(), name='create_report'),
+    path('/comments/<int:pk>/reports/', get_comment_reports, 'get_comment_reports'),
+    path('/books/<int:pk>/comments/', get_book_comments, name='get_book_comments'),
+    path('/books/<int:pk>/like/', CreateLike.as_view(), name='create_like'),
 ]
