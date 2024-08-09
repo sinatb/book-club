a03dc80193d3b0a185c5f896d433df5d8081851b
sinatb <sinatb.dev@gmail.com>
2024-08-09 10:42:49 +0330

    bookclub-021 : corrected small error in paths

--- bookclubapi/urls.py
+++ bookclubapi/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('/comments/', CommentCreate.as_view(), name='create_comment'),
     path('/comments/<int:pk>/', CommentDetail.as_view(), name='get_comment_detail'),
     path('/reports/', ReportCreate.as_view(), name='create_report'),
-    path('/comments/<int:pk>/reports/', get_comment_reports, 'get_comment_reports'),
+    path('/comments/<int:pk>/reports/', get_comment_reports, name='get_comment_reports'),
     path('/books/<int:pk>/comments/', get_book_comments, name='get_book_comments'),
     path('/books/<int:pk>/like/', CreateLike.as_view(), name='create_like'),
 ]
