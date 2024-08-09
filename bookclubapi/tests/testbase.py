9b054e12ea009f7a99ed760d14b73641b33ea656
sinatb <sinatb.dev@gmail.com>
2024-08-09 11:32:47 +0330

    bookclub-024 : userbase bug solved

--- bookclubapi/tests/testbase.py
+++ bookclubapi/tests/testbase.py
@@ -1,5 +1,4 @@
 from bookclubapi.models import User
-from rest_framework.authtoken.models import Token
 from django.test import TestCase
 
 
@@ -12,5 +11,13 @@ class BookClubTestBase(TestCase):
             email=f'{username}@mail.com',
             user_type='publisher'
         )
-        token = Token.objects.create(user=user)
-
+        return user
+    @staticmethod
+    def get_basic(username, password):
+        user = User.objects.create_user(
+            username=username,
+            password=password,
+            email=f'{username}@mail.com',
+            user_type='basic'
+        )
+        return user
