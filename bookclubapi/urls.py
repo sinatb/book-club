67242b53e199f712aad80410cabbb042453a9640
sinatb <sinatb.dev@gmail.com>
2024-08-09 17:30:06 +0330

    bookclub-026 : deleted redundant / from urls

--- bookclubapi/urls.py
+++ bookclubapi/urls.py
@@ -3,12 +3,12 @@ from .apiviews import (BookList, BookDetail, CommentCreate, CommentDetail, Repor
                        get_book_comments, get_comment_reports, CreateLike)
 
 urlpatterns = [
-    path('/books/', BookList.as_view(), name='get_book_list'),
-    path('/books/<int:pk>/', BookDetail.as_view(), name='get_book_detail'),
-    path('/comments/', CommentCreate.as_view(), name='create_comment'),
-    path('/comments/<int:pk>/', CommentDetail.as_view(), name='get_comment_detail'),
-    path('/reports/', ReportCreate.as_view(), name='create_report'),
-    path('/comments/<int:pk>/reports/', get_comment_reports, name='get_comment_reports'),
-    path('/books/<int:pk>/comments/', get_book_comments, name='get_book_comments'),
-    path('/books/<int:pk>/like/', CreateLike.as_view(), name='create_like'),
+    path('books/', BookList.as_view(), name='get_book_list'),
+    path('books/<int:pk>/', BookDetail.as_view(), name='get_book_detail'),
+    path('comments/', CommentCreate.as_view(), name='create_comment'),
+    path('comments/<int:pk>/', CommentDetail.as_view(), name='get_comment_detail'),
+    path('reports/', ReportCreate.as_view(), name='create_report'),
+    path('comments/<int:pk>/reports/', get_comment_reports, name='get_comment_reports'),
+    path('books/<int:pk>/comments/', get_book_comments, name='get_book_comments'),
+    path('books/<int:pk>/like/', CreateLike.as_view(), name='create_like'),
 ]
