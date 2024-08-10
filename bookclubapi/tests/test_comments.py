db9879d8e131ac64cef73961b6d9e9d5a1fea4d3
sinatb <sinatb.dev@gmail.com>
2024-08-10 15:45:24 +0330

    comment-apis-002 : comment create and detail apis implemented

--- bookclubapi/tests/test_comments.py
+++ bookclubapi/tests/test_comments.py
@@ -2,7 +2,7 @@ import datetime
 
 from rest_framework import status
 from bookclubapi.models import Book, Comment
-from bookclubapi.serializers import BookSerializer
+from bookclubapi.serializers import CommentSerializer
 from .fixture import BookClubFixture
 
 # Create your tests here.
@@ -28,7 +28,7 @@ class CommentAPITests(BookClubFixture):
             'book': self.b1.pk,
             'content': 'test comment'
         })
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_create_comment_success(self):
         self.client.force_authenticate(user=self.commentator)
@@ -41,25 +41,30 @@ class CommentAPITests(BookClubFixture):
         self.client.force_authenticate(user=None)
 
     def test_get_comment(self):
+        self.client.force_authenticate(user=self.commentator)
         response = self.client.get(f'/comments/{self.c1.pk}/')
         comment = Comment.objects.get(id=self.c1.pk)
-        serializer = BookSerializer(comment)
+        serializer = CommentSerializer(comment)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(response.data, serializer)
         self.assertEqual(response.data, serializer.data)
+        self.client.force_authenticate(user=None)
 
     def test_delete_comment_fail(self):
         response = self.client.delete(f'/comments/{self.c1.pk}/')
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_delete_comment_fail_not_self(self):
-        self.client.force_authenticate(user=self.user)
-        response = self.client.delete(f'/comments/{self.c1.pk}/')
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
-    def test_delete_comment_success(self):
         self.client.force_authenticate(user=self.commentator)
         response = self.client.delete(f'/comments/{self.c1.pk}/')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.client.force_authenticate(user=None)
+
+    def test_delete_comment_success(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.delete(f'/comments/{self.c1.pk}/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.client.force_authenticate(user=None)
 
     def test_get_comments_reports_unauthorized(self):
         response = self.client.get(f'/comments/{self.c1.pk}/reports/')
