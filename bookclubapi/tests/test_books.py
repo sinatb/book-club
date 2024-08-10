5f842ca718817783179277d0c4f124ed0e03f305
sinatb <sinatb.dev@gmail.com>
2024-08-10 08:36:40 +0330

    bookclub-028 : updated book tests

--- bookclubapi/tests/test_books.py
+++ bookclubapi/tests/test_books.py
@@ -1,6 +1,7 @@
 import datetime
 
 from rest_framework.test import APIClient
+from rest_framework import status
 from bookclubapi.models import Book
 from bookclubapi.serializers import BookSerializer
 from .testbase import BookClubTestBase
@@ -14,28 +15,28 @@ class BookAPITests(BookClubTestBase):
     def setUp(self):
         self.user = BookClubTestBase.get_publisher('publisher', 'password')
         self.b1 = Book.objects.create(name="Test Book",
-                                      publisher_id=self.user.id,
+                                      publisher_id=self.user.pk,
                                       publish_date=datetime.date(2020, 1, 1),
                                       Genre="test")
 
         self.b2 = Book.objects.create(name="Test Book 2",
-                                      publisher_id=self.user.id,
+                                      publisher_id=self.user.pk,
                                       publish_date=datetime.date(2023, 1, 1),
                                       Genre="test2")
 
         self.b3 = Book.objects.create(name="Test Book 3",
-                                      publisher_id=self.user.id,
+                                      publisher_id=self.user.pk,
                                       publish_date=datetime.date(2025, 1, 1),
                                       Genre="test")
 
         self.b4 = Book.objects.create(name="Test Book 4",
-                                      publisher_id=self.user.id,
+                                      publisher_id=self.user.pk,
                                       publish_date=datetime.date(2003, 1, 1),
                                       Genre="test2")
 
     def test_get_books(self):
         response = client.get('/books/')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         books = Book.objects.all()
         serializer = BookSerializer(books, many=True)
@@ -43,50 +44,50 @@ class BookAPITests(BookClubTestBase):
         self.assertEqual(response.data, serializer.data)
 
     def test_get_book(self):
-        response = client.get('/books/3/')
-        book = Book.objects.get(pk=self.b3.pk)
+        response = client.get(f'/books/{self.b3.pk}/')
+        book = Book.objects.get(id=self.b3.pk)
         serializer = BookSerializer(book)
         self.assertEqual(response.data, serializer.data)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_get_unavailable_book(self):
         response = client.get('/books/30/')
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_post_book_unauthorized(self):
         response = client.post('/books/', data={
             'name': "Test Book 5",
-            'publisher_id': self.user.id,
+            'publisher_id': self.user.pk,
             'publish_date': datetime.date(2033, 4, 3),
             'Genre': 'test3'
         })
-        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_post_book_success(self):
         client.force_authenticate(user=self.user)
         response = client.post('/books/', data={
             'name': "Test Book 2",
-            'publisher_id': self.user.id,
+            'publisher_id': self.user.pk,
             'publish_date': datetime.date(2033, 4, 3),
             'Genre': 'test2'
         })
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         client.force_authenticate(user=None)
 
     def test_delete_book_unauthorized(self):
-        response = client.delete('/books/3/')
-        self.assertEqual(response.status_code, 401)
+        response = client.delete(f'/books/{self.b3.pk}/')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_delete_book_success(self):
         client.force_authenticate(user=self.user)
-        response = client.delete('/books/3/')
-        self.assertEqual(response.status_code, 200)
+        response = client.delete(f'/books/{self.b3.pk}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertFalse(Book.objects.filter(pk=self.b3.pk).exists())
         client.force_authenticate(user=None)
 
     def test_delete_missing_book(self):
         client.force_authenticate(user=self.user)
         response = client.delete('/books/30/')
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
         self.assertFalse(Book.objects.filter(pk=self.b3.pk).exists())
         client.force_authenticate(user=None)
